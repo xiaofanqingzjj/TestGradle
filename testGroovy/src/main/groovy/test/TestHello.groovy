befb2c630e0ac2b0b173adaa30718559370546a8
xiaofanqing <258013207@qq.com>
2020-03-08 20:27:37 +0800

    init

---
+++ testGroovy/src/main/groovy/test/TestHello.groovy
@@ -0,0 +1,55 @@
+
+// groovy默认导入了java.lang*、java.util.*、java.net*，所以一些基本的类可以不需要导入
+
+package test
+
+class TestHello {
+
+    static void main(String[] args) {
+        println "Hello world"  // 尾巴上的分号可以不要，方法的括号可以省略
+
+
+        // 数据类型，和java一样
+        int x = 5
+        long lv = 100l
+        float f = 1.1f
+        double dv = 111.00
+
+        // 变量的定义有两种方法， 一种是
+        def va = 33
+//        def
+
+        // groovy中的循环
+
+        //  从0循环到3
+        0.upto(3) {
+            print(it)
+        }
+
+        println("\n---")
+        // 循环4次，从0到3
+        4.times {
+            print(it)
+        }
+
+        // 使用java的方式执行一条命令行程序
+        println(executeCommand("ls -l"))
+
+
+
+    }
+
+    // execute command line in java
+    static String executeCommand(String command) {
+        final Process process = Runtime.getRuntime().exec(command)
+        BufferedReader br = new BufferedReader(new InputStreamReader(process.getInputStream()))
+
+        StringBuffer sb = new StringBuffer()
+        String line
+        while((line = br.readLine()) != null) {
+            sb.append("$line\n")
+        }
+        return sb.toString()
+    }
+
+}
---
+++ testGroovy/src/main/java/com/testlib/Greeting.java
@@ -0,0 +1,8 @@
+package com.testlib;
+
+public class Greeting {
+
+    public static String greeting(String name) {
+        return "Hello:" + name;
+    }
+}
