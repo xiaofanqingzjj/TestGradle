753a0cc077458559ee6adc4411c9c65f243e2cf5
xiaofanqing <xiaofanqingzjj@163.com>
2020-04-07 22:56:21 +0800

    修改签名脚本

--- sign/sign.sh
+++ sign/sign.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-if [ $# -lt 2 ]; then
-    echo "Usage: sign <input.apk> <output.apk>"
+if [ $# -lt 1 ]; then
+    echo "Usage: sign <input.apk>"
     exit 1
 fi
 
 # password tciegxy
 
 inputFile=$1
-outputFile=$2
+outputFile="${inputFile%.*}_signed.apk"
 
 
 if [ -z ${ANDROID_HOME} ]; then #先判断是否有ANDROID_HOME环境变量
@@ -23,5 +23,9 @@ apkSigner=$ANDROID_HOME/build-tools/25.0.0/apkSigner
 echo "input:$inputFile, outputFile:$outputFile"
 
 ${apkSigner} sign  --ks xy.keystore --out ${outputFile} -v --v1-signing-enabled true --v2-signing-enabled false ${inputFile}
+
+# auto input password
+
+
 
 ${apkSigner} verify ${outputFile}
